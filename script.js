d3beeff9211cb5b364d7d71056ecc32ff337fc03
viniciusalmeida26-blip <vinicius.almeida26@escola.pr.gov.br>
2026-06-24 12:31:26 -0300

    Criação de conexão com arquivo local Json

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const form = document.getElementById('form')
 const input = document.getElementsByClassName('input')
 const todoUl = document.getElementById('todo')
+
+const todos = JSON.parse(localStorage.getItem('todos'))
